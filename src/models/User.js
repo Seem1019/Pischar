837bc38c1ee63d7d46a69b318e122120b0cc0a57
Steven Espinoza <stevene@uninorte.edu.co>
2022-05-25 18:54:42 -0500

    "cambios menores"

--- src/models/User.js
+++ src/models/User.js
@@ -2,10 +2,13 @@ import mongoose from "mongoose";
 
 const { Schema, model } = mongoose;
 
-const user = new Schema({
-  display_name: { type: String, required: true },
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-},{versionKey: false});
+const user = new Schema(
+  {
+    display_name: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  { versionKey: false }
+);
 
 export default model("User", user);
