5b341b2e4c0e5a08c01764d771ba629c3b19b444
Steven Espinoza <stevene@uninorte.edu.co>
2022-05-25 19:47:21 -0500

    "Feat: adding new properties from user model"

--- src/models/User.js
+++ src/models/User.js
@@ -7,6 +7,9 @@ const user = new Schema(
     display_name: { type: String, required: true },
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    bio: { type: String, required: false },
+    birthDate: { type: Date, required: true },
   },
   { versionKey: false }
 );
