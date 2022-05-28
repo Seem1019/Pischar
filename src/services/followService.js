3f52593c7dbbf8dff8bd6f855fa66abf905fa78e
VSoulRP <vsrp97@hotmail.com>
2022-05-27 22:28:30 -0500

    Pretty

--- src/services/followService.js
+++ src/services/followService.js
@@ -1,4 +1,4 @@
-import {getUserId} from "../utils/jwt";
+import { getUserId } from "../utils/jwt";
 import follow_model from "../models/Follow.js";
 import user_model from "../models/User.js";
 
@@ -9,7 +9,7 @@ export const fetchFollowers = async (req, res) => {
     const following = await follow_model.findOne({
       followed_id: user_id,
       follower_id: logged_user_id,
-      accepted: true
+      accepted: true,
     });
     if (following || user_id === logged_user_id) {
       const follower_users = await follow_model.find({ followed_id: user_id });
@@ -32,7 +32,7 @@ export const fetchFollowing = async (req, res) => {
     const following = await follow_model.findOne({
       followed_id: user_id,
       follower_id: logged_user_id,
-      accepted: true
+      accepted: true,
     });
     if (following || user_id === logged_user_id) {
       const followed_users = await follow_model.find({ follower_id: user_id });
@@ -55,19 +55,19 @@ export const requestFollow = async (req, res) => {
     if (user_id !== logged_user_id) {
       const request = await follow_model.findOne({
         followed_id: user_id,
-        follower_id: logged_user_id
+        follower_id: logged_user_id,
       });
       if (!request) {
         await follow_model.create({
           followed_id: user_id,
-          follower_id: logged_user_id
+          follower_id: logged_user_id,
         });
       } else {
         return res.status(400).json({ message: "Request already exists." });
       }
       return res.status(201).json({});
     }
-    return res.status(400).json({message: "Can't follow oneself."});
+    return res.status(400).json({ message: "Can't follow oneself." });
   } catch (error) {
     return res.status(500).json(error);
   }
@@ -80,7 +80,7 @@ export const requestResponse = async (req, res) => {
     const request = await follow_model.findOne({
       followed_id: logged_user_id,
       follower_id: request_id,
-      accepted: false
+      accepted: false,
     });
     if (action === "accept") {
       request.accepted = true;
